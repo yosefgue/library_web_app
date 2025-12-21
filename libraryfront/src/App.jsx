3cd77ef5a151151bf27223e6ba6abd366c4ab934
yosefgue <yosefgue6@gmail.com>
2025-12-21 16:33:54 +0100

    project initialization

---
+++ libraryfront/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from '@mantine/core';
+
+function App() {
+  return (
+    <>
+      <Button variant="filled">Button</Button>
+    </>
+  )
+}
+
+export default App
